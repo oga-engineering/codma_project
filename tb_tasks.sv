41462bcc6d99c878390da9639bc7cf0d6b12aa1a
Oliver <ollie.anderson27@icloud.com>
2023-03-21 11:24:43 +0000

    Version 1
    Version one.
    Removed contentions to drive the bus
    Confirmed operation of tasks 0 1 2 and the error handling
    The status is updated and the irq is asserted for one cycle at the end of the operation.
    This code will be sent to infineon.

--- tb_tasks.sv
+++ tb_tasks.sv
@@ -1,4 +1,16 @@
-// Set of tasks used by the testbench
+/*
+Oliver Anderson
+Univeristy of Bath
+codma FYP 2023
+
+Set of Tasks used by the testbench (codma_tb.sv). Tasks include a setup data task that sets up the memory array
+and a task to verify the data has been copied from the correct location to the correct destination location and the number
+of copied bytes match up.
+*/
+
+//=======================================================================================
+// SETUP THE POINTERS IN THE MEM ARRAY
+//=======================================================================================
 task setup_data(
     input [31:0]    task_pointer,
     input [31:0]    task_type,
@@ -16,31 +28,28 @@ task setup_data(
 	    logic [31:0] dest_addr;
 
         // this randomisation method needs refining. May causes clashes.
-        source_addr     = $urandom_range(0,(inst_mem.MEM_DEPTH-4))*inst_mem.MEM_WIDTH;
+        source_addr     = $urandom_range(0,((inst_mem.MEM_DEPTH*inst_mem.MEM_WIDTH)-(len_bytes)));
         source_addr_o   = source_addr;
         // add +32 so it doesn not overwrite the linked task. Not the smartest way, but it works
-        dest_addr       = ($urandom_range(0,(inst_mem.MEM_DEPTH-4))*inst_mem.MEM_WIDTH)+32;
+        dest_addr       = $urandom_range(0,((inst_mem.MEM_DEPTH*inst_mem.MEM_WIDTH)-(len_bytes)))/*+32*/;
         dest_addr_o     = dest_addr;
 
         // assign to shape for memory module
         inst_mem.mem_array[(task_pointer/inst_mem.MEM_WIDTH)] = {source_addr,task_type};
         inst_mem.mem_array[((task_pointer/inst_mem.MEM_WIDTH)+1)] = {len_bytes,dest_addr};
 
-        // Make a copy of the mem_array before changes (avoid overlap verification issues)
-        int_mem = inst_mem.mem_array;
-
         // SETUP A LINK TASK 
         if (task_type == 'd2) begin
-            source_addr     = $urandom_range(0,(inst_mem.MEM_DEPTH-4))*inst_mem.MEM_WIDTH;
-            source_addr_l   = source_addr;
-            dest_addr       = ($urandom_range(0,(inst_mem.MEM_DEPTH-4))*inst_mem.MEM_WIDTH);
-            dest_addr_l     = dest_addr;
-            task_type_l     = $urandom_range(1,1);
+            task_type_l     = 0;
             if (task_type_l == 'd0) begin
-                len_bytes_l     = ($urandom_range(1,4)*8);
+                len_bytes_l = ($urandom_range(1,4)*8);
             end else begin
                 len_bytes_l = ($urandom_range(1,3)*32);
             end
+            source_addr     = $urandom_range(0,((inst_mem.MEM_DEPTH*inst_mem.MEM_WIDTH)-(len_bytes_l)));
+            source_addr_l   = source_addr;
+            dest_addr       = $urandom_range((dest_addr_o+len_bytes),((inst_mem.MEM_DEPTH*inst_mem.MEM_WIDTH)-(len_bytes_l)));
+            dest_addr_l     = dest_addr;
             inst_mem.mem_array[(task_pointer/inst_mem.MEM_WIDTH)+4] = {source_addr,task_type_l};
             inst_mem.mem_array[((task_pointer/inst_mem.MEM_WIDTH)+5)] = {len_bytes_l,dest_addr};
         end else begin
@@ -49,14 +58,20 @@ task setup_data(
             len_bytes_l     = 'd0;
             task_type_l     = 'd0;
         end
-        
+
+        // Make a copy of the mem_array in case a source and destination address overlap (eg in a 4 write burst)
+        int_mem = inst_mem.mem_array;
 
     end
 endtask
 
+//=======================================================================================
+// TEST THE DATA
+//=======================================================================================
 task check_data(
-    input [31:0] source_addr_o,
-    input [31:0] dest_addr_o,
+    input        status_pointer,
+    input [31:0] source_addr,
+    input [31:0] dest_addr,
     input [31:0] source_addr_l,
     input [31:0] dest_addr_l,
     input [31:0] task_type,
@@ -75,41 +90,47 @@ task check_data(
 
         
 
-        src_location = source_addr_o/inst_mem.MEM_WIDTH;
-        dst_location = dest_addr_o/inst_mem.MEM_WIDTH;
-        #50
-        if (task_type == 'd0) begin
+        src_location = source_addr/inst_mem.MEM_WIDTH;
+        dst_location = dest_addr/inst_mem.MEM_WIDTH;
+        #10
+        if ( (source_addr + len_bytes) > (inst_mem.MEM_DEPTH*inst_mem.MEM_WIDTH) || (dest_addr + len_bytes) > (inst_mem.MEM_DEPTH*inst_mem.MEM_WIDTH)) begin
+            if (inst_mem.mem_array[status_pointer] == 'd1) begin
+                $display("Task type %d succesfully flagged an error - mem addr did not exist",task_type);
+            end else begin
+                $display("Task type %d did not flag error - but should have: mem addr did not exist",task_type);
+            end
+        end else if (task_type == 'd0) begin
             for (int i=0; i<(len_bytes/8); i++ ) begin
-                if (int_mem[src_location] === inst_mem.mem_array[dst_location]) begin
+                if (int_mem[src_location] == inst_mem.mem_array[dst_location]) begin
                     pass_message(task_type, len_bytes,i);
                     src_location = src_location + i;
                     dst_location = dst_location + i;
                 end else begin
-                    fail_message(len_bytes, task_type, src_location, dst_location);
+                    fail_message(len_bytes, task_type, src_location, dst_location,int_mem);
                     src_location = src_location + i;
                     dst_location = dst_location + i;
                 end
             end
         end else if (task_type == 'd1) begin
             for (int i=0; i<(len_bytes/32); i++ ) begin
-                if (int_mem[src_location] === inst_mem.mem_array[dst_location]) begin
+                if (int_mem[src_location] == inst_mem.mem_array[dst_location]) begin
                     pass_message(task_type, len_bytes,i);
                     src_location = src_location + i;
                     dst_location = dst_location + i;
                 end else begin
-                    fail_message(len_bytes, task_type, src_location, dst_location);
+                    fail_message(len_bytes, task_type, src_location, dst_location,int_mem);
                     src_location = src_location + i;
                     dst_location = dst_location + i;
                 end
             end
         end else if (task_type == 'd2) begin
             for (int i=0; i<(len_bytes/32); i++ ) begin
-                if (int_mem[src_location] === inst_mem.mem_array[dst_location]) begin
+                if (int_mem[src_location] == inst_mem.mem_array[dst_location]) begin
                     pass_message(task_type, len_bytes,i);
                     src_location = src_location + i;
                     dst_location = dst_location + i;
                 end else begin
-                    fail_message(len_bytes, task_type, src_location, dst_location);
+                    fail_message(len_bytes, task_type, src_location, dst_location,int_mem);
                     src_location = src_location + i;
                     dst_location = dst_location + i;
                 end
@@ -117,25 +138,38 @@ task check_data(
             // check the linked task
             $display("Check Linked Task: task type %d",task_type_l);
             check_data(
-				source_addr_l,   //source_addr_o,
-				dest_addr_l,     //dest_addr_o,
+                status_pointer,
+				source_addr_l,   //source_addr,
+				dest_addr_l,     //dest_addr,
                 'd0,             //source_addr_l,
                 'd0,             //dest_addr_l,
                 task_type_l,     //task_type,
 				len_bytes_l,     //len_bytes,
                 'd0,             //task_type_l,
                 'd0,             //len_bytes_l,
-				int_mem         //int_mem
+				int_mem        //int_mem
 			);
         end
+
+        // check the status
+        if (inst_mem.mem_array[status_pointer] == 'd0) begin
+            $display("status pointer updated correctly %d",'d0);
+        end else begin
+            $display("status pointer incorrect ; not %d",'d0);
+        end
+
     end
 endtask
 
+//=======================================================================================
+// PRINT A FAIL MESSAGE (HELPS THE DEBUG)
+//=======================================================================================
 task fail_message(
     input [31:0] len_bytes,
     input [31:0] task_type,
     input [31:0] src_location,
-    input [31:0] dst_location
+    input [31:0] dst_location,
+    input [31:0][7:0][7:0] int_mem
     );
     begin
     $display("test failed: test type %d ; len_bytes %d", task_type, len_bytes);
@@ -145,6 +179,9 @@ task fail_message(
     end
 endtask
 
+//=======================================================================================
+// PRINT A PASS MESSAGE (HELPS CELEBRATE. WOO.)
+//=======================================================================================
 task pass_message(
     input [31:0] task_type,
     input [31:0] len_bytes,
--- top.sv
+++ top.sv
@@ -1,5 +1,18 @@
+/*
+Oliver Anderson
+Univeristy of Bath
+codma FYP 2023
+
+Top level module file for the codma. This file connects the codma, read and write machine modules.
+It is the ONLY module to drive the bus interface signals to avoid contentions.
+It will contain the assertions to confirm the state machines do not fall into unknown states. Though
+the unknown states will be defined for a "belt and braces" approach to eliminate this as a point of failure.
+*/
 `include "machine_pkg.sv"
 
+//=======================================================================================
+// CODMA MODULE START
+//=======================================================================================
 module ip_codma_top
 #()(
     // clock and reset
@@ -21,24 +34,20 @@ module ip_codma_top
 
 );
 
-//--------------------------------------------------
+//=======================================================================================
 // INTERNAL SIGNALS AND MARKERS
-//--------------------------------------------------
+//=======================================================================================
 
-logic [31:0] reg_addr;
-logic [7:0]  reg_size;
+logic [31:0] reg_addr, reg_addr_wr;
+logic [7:0]  reg_size, reg_size_wr;
 logic need_read_i, need_read_o;
 logic need_write_i, need_write_o;
+logic [7:0] write_count_s;
 
 logic [7:0][31:0] data_reg;
 logic [7:0][31:0] write_data;
 logic [7:0][31:0] crc_code;
 
-logic [31:0] len_bytes;
-
-//--------------------------------------------------
-// STATE MACHINES
-//--------------------------------------------------
 read_pkg::read_state_t      rd_state_r;
 read_pkg::read_state_t      rd_state_next_s; 
 write_pkg::write_state_t    wr_state_r;      
@@ -46,11 +55,13 @@ write_pkg::write_state_t    wr_state_next_s;
 dma_pkg::dma_state_t        dma_state_r;
 dma_pkg::dma_state_t        dma_state_next_s;
 
+//=======================================================================================
+// CONNECT THE MODULES
+//=======================================================================================
+
 read_machine inst_rd_machine(
     .clk_i(clk_i),
     .reset_n_i(reset_n_i),
-    .reg_addr(reg_addr),
-    .reg_size(reg_size),
     .need_read_i(need_read_i),
     .need_read_o(need_read_o),
     .rd_state_r(rd_state_r),
@@ -62,13 +73,12 @@ read_machine inst_rd_machine(
 write_machine inst_wr_machine(
     .clk_i(clk_i),
     .reset_n_i(reset_n_i),
-    .reg_addr(reg_addr),
-    .reg_size(reg_size),
     .need_write_i(need_write_i),
     .need_write_o(need_write_o),
-    .data_reg(write_data),
+    .word_count_wr(write_count_s),
     .wr_state_r(wr_state_r),
     .wr_state_next_s(wr_state_next_s),
+    .rd_state_next_s(rd_state_next_s),
     .bus_if(bus_if)
 );
 
@@ -76,17 +86,20 @@ codma_machine inst_dma_machine(
     .clk_i(clk_i),
     .reset_n_i(reset_n_i),
     .start_i(start_i),
+    .stop_i(stop_i),
     .busy_o(busy_o),
     .irq_o(irq_o),
     .task_pointer_i(task_pointer_i),
+    .status_pointer_i(status_pointer_i),
     .rd_state_next_s(rd_state_next_s),
     .rd_state_r(rd_state_r),
     .wr_state_next_s(wr_state_next_s),
-    .wr_state_r(wr_state_r),    
     .dma_state_next_s(dma_state_next_s),
     .dma_state_r(dma_state_r),
     .reg_addr(reg_addr),
     .reg_size(reg_size),
+    .reg_addr_wr(reg_addr_wr),    
+    .reg_size_wr(reg_size_wr),    
     .need_read_i(need_read_i),
     .need_read_o(need_read_o),
     .need_write_i(need_write_i),
@@ -103,9 +116,55 @@ compute_crc inst_compute_crc (
     .crc_output(crc_code)
 );
 
+//=======================================================================================
+//      DRIVE THE BUS. BRUM BRUM
+//      .-------------------------------------------------------------.
+//      '------..-------------..----------..----------..----------..--.|
+//      |       \\            ||          ||          ||          ||  ||
+//      |        \\           ||          ||          ||          ||  ||
+//      |    ..   ||  _    _  ||    _   _ || _    _   ||    _    _||  ||
+//      |    ||   || //   //  ||   //  // ||//   //   ||   //   //|| /||
+//      |_.------"''----------''----------''----------''----------''--'|
+//       |)|      |       |       |       |    |         |      ||==|  |
+//       | |      |  _-_  |       |       |    |  .-.    |      ||==| C|
+//       | |  __  |.'.-.' |   _   |   _   |    |.'.-.'.  |  __  | "__=='
+//       '---------'|( )|'----------------------'|( )|'----------""
+//                   '-'                          '-'
+//=======================================================================================
+always_comb begin
+    // Standard Values
+    bus_if.read         = 'd0;
+    bus_if.write        = 'd0;
+    bus_if.write_valid  = 'd0;
+    bus_if.write_data   = 'd0;
+    bus_if.size         = 'd9;
+    bus_if.write_data   = {write_data[write_count_s+1],write_data[write_count_s]};
+
+    // Wants to Read
+    if (rd_state_r == read_pkg::RD_ASK) begin
+        bus_if.read     = 'd1;
+        bus_if.size     = reg_size;
+        bus_if.addr     = reg_addr;
+    end else if (rd_state_r == read_pkg::RD_GRANTED) begin
+        bus_if.size     = reg_size;
+        bus_if.addr     = reg_addr;
+    // Wants to Write
+    end else if (wr_state_r == write_pkg::WR_ASK) begin
+        bus_if.write        = 'd1;
+        bus_if.size         = reg_size_wr;
+        bus_if.addr         = reg_addr_wr;
+    end else if (wr_state_r == write_pkg::WR_GRANTED) begin
+        bus_if.size         = reg_size_wr;
+        bus_if.addr         = reg_addr_wr;
+        bus_if.write_valid  = 'd1;
+    end 
+end
+
+//=======================================================================================
 // ASSERTIONS (NOT FUNCTIONNING)
-assert property (
-    @(clk_i) (dma_state_next_s == dma_pkg::DMA_COMPUTE)
-) else $error("TEST ERROR");
+//=======================================================================================
+//assert property (
+//    @(clk_i) (dma_state_next_s == dma_pkg::DMA_COMPUTE)
+//) else $error("TEST ERROR");
 
 endmodule
