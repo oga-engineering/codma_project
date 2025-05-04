33c1b686981d58e31115eee63f4fd7febeacecc2
Oliver Anderson <oliveranderson@Olivers-MacBook-Air.local>
2025-05-04 13:49:56 +0100

    Running first test cocotb testbench with makefile

--- cocotb_testbench/freerun_testbench.py
+++ cocotb_testbench/freerun_testbench.py
@@ -1,20 +1,14 @@
 import cocotb
-from cocotb.clock       import Clock
-from cocotb.triggers    import RisingEdge, Timer
+from cocotb.triggers import Timer
 
-@cocotb.coroutine
-async def free_run(ip_codma_top):
-    
-    # Create a 10ns period clock on ip_codma_top.clk
-    cocotb.start_soon(Clock(ip_codma_top.clk_i, 10, units="ns").start())
 
-    # Apply reset
-    ip_codma_top.rst_n_i.value = 0  # Active-low reset
-    ip_codma_top._log.info("Reset asserted")
-    await Timer(20, units="ns")  # Hold reset for 20ns
-    ip_codma_top.rst_n_i.value = 1
-    ip_codma_top._log.info("Reset deasserted")
+@cocotb.test()
+async def my_first_test(dut):
+    """Try accessing the design."""
 
-    # Wait a few clock cycles
-    for _ in range(5):
-        await RisingEdge(ip_codma_top.clk_i)
+    for cycle in range(10):
+        dut.clk_i.value = 0
+        await Timer(1, units="ns")
+        dut.clk_i.value = 1
+        await Timer(1, units="ns")
+
